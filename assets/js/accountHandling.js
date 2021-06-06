fb8285713457b71fd335c9507e4f43789c0a6b10
jankubatt <jankubat.1@seznam.cz>
2021-06-06 17:41:23 +0200

    error register messages

--- assets/js/accountHandling.js
+++ assets/js/accountHandling.js
@@ -32,20 +32,34 @@ function login() {
         if (data == "OK") {
             window.location.replace("../home.html");
         }
+        else {
+            $("#error").html("Špatné údaje");
+        }
     });
 }
 
 function register() {
-    if ($("#pass").val() == $("#passAgain").val())
-        $.post("assets/php/registerScript.php", {login: $("#login").val(), pass: $("#pass").val()}).then(function(data) {
-            if (data == "OK") {
-                window.location.replace("../index.html");
+    if (validateRegister()) {
+        $.get("assets/php/checkUserExists.php", {user: $("#login").val()}).then(function(data) {
+            if(data.includes("true")) {
+                $("#error").html("Neplatné jméno");
             }
-        });
-
-    else {
-        $("#error").html("Hesla nejsou stejná");
+            else {
+                $.post("assets/php/registerScript.php", {login: $("#login").val(), pass: $("#pass").val()}).then(function(data) {
+                    if (data.includes("OK")) {
+                        window.location.replace("../index.html");
+                    }
+                });
+            }
+        })
     }
+}
+
+function validateRegister() {
+    if ($("#login").val().length < 3) { $("#error").html("Jméno musí mít více jak 3 znaky"); return false; }
+    if ($("#pass").val().length < 8) { $("#error").html("Heslo musí být delší jak 8 znaků."); return false; }
+    if ($("#pass").val() != $("#passAgain").val()) { $("#error").html("Hesla nejsou stejná."); return false; }
+    return true;
 }
 
 function getCookie(cname) {
--- assets/php/getUserData.php
+++ assets/php/getUserData.php
@@ -7,22 +7,22 @@
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-        $id = $row['id'];
-    }
+        while($row = mysqli_fetch_assoc($result)) {
+            $id = $row['id'];
+        }
     }
 
     $sql = "SELECT login, wasHere FROM users WHERE id='$id'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-        $login = $row['login'];
-        $wasHere = $row['wasHere'];
-        $myObj = array("id" => $id, "login" => $login, "wasHere" => $wasHere);
-        $myJSON = json_encode($myObj);
-        echo $myJSON;
-    }
+        while($row = mysqli_fetch_assoc($result)) {
+            $login = $row['login'];
+            $wasHere = $row['wasHere'];
+            $myObj = array("id" => $id, "login" => $login, "wasHere" => $wasHere);
+            $myJSON = json_encode($myObj);
+            echo $myJSON;
+        }
     }
 
     mysqli_close($conn);
