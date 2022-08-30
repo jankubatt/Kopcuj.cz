e310ea2e1fa18192c8a6df0c2d818ad606277657
jankubatt <jankubat77@gmail.com>
2022-08-30 14:45:21 +0200

    feat(app): Added back react-mapycz, frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,43 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {KeyboardControl, Map, Marker, MarkerLayer, MouseControl, ZoomControl} from "react-mapycz";
+import axios from "axios";
 
-import Register from "./pages/register";
-import Login from "./pages/login";
-import Map from "./pages/map";
+function App() {
+    const [hills, getHills] = useState([]);
 
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <Routes>
-                    <Route exact path='/' element={<Map/>}/>
-                    <Route path='/register' element={<Register/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                </Routes>
-            </Router>
-        );
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('http://localhost:8082/api/hills/');
+            return response.data;
+        }
+        fetchData()
+            .then((res) => {
+                getHills(res)
+            })
+            .catch((e) => {
+                console.log(e.message)
+            })
+    }, [])
+
+    return (
+        <Map height={"100vh"} center={{lat: 50.555, lng: 13.931}} zoom={14}>
+            <KeyboardControl/>
+            <ZoomControl/>
+            <MouseControl zoom={true} pan={true} wheel={true}/>
+            <MarkerLayer>
+                {hills.map((hill) => <Marker key={hill._id} coords={{lat: hill.lat, lng: hill.lon}} card={{
+                    header: () => <strong>{hill.name}</strong>,
+                    body: () => <><p>{hill.elevation}m</p></>,
+                    footer: `${hill.location}`,
+                    options: {
+                        width: 300,
+                        height: 300,
+                    }
+                }}></Marker>)}
+            </MarkerLayer>
+        </Map>
+    );
 }
 
 export default App;
