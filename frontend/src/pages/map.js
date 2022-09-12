40d2f71eae6d9d7066f41f65e80ab81b427ebe25
jankubatt <jankubat77@gmail.com>
2022-09-12 17:00:08 +0200

    feat(app): reviews, part 4

--- frontend/src/pages/map.js
+++ frontend/src/pages/map.js
@@ -3,8 +3,8 @@ import '../App.css';
 import axios from "axios";
 import {KeyboardControl, Map, Marker, MarkerLayer, MouseControl, ZoomControl} from 'react-mapycz'
 import Cookies from 'js-cookie';
-import Sidebar from "../components/Sidebar";
 import '../Map.css';
+import {Rating} from "@mui/material";
 
 axios.defaults.withCredentials = true;
 
@@ -14,11 +14,28 @@ function MapPage() {
     const [currentHill, setCurrentHill] = useState();
     const [center, setCenter] = useState(true)
     const [climbed, setClimbed] = useState(true);
+    const [rating, setRating] = useState(0);
 
     //Check if user is logged in. If not, redirect user to login page
     let authToken = Cookies.get('authToken');
     if (authToken === '' || authToken === undefined || authToken === null) {
         document.location.replace(document.location + 'login');
+    }
+
+    async function addHill() {
+        await axios.post('http://localhost:8082/api/users/addHill', {
+            authToken: Cookies.get('authToken'),
+            hillId: currentHill._id
+        });
+    }
+
+    async function sendRating() {
+        await axios.post(`http://localhost:8082/api/review/`, {
+            stars: rating,
+            hillId: currentHill._id,
+            userId: user._id,
+            text: null
+        });
     }
 
     const fetchUser = async () => {
@@ -59,8 +76,12 @@ function MapPage() {
             const clickedHill = await axios.get(`http://localhost:8082/api/hills/name/${hillName}`);
 
             const hillReviews = await axios.get(`http://localhost:8082/api/review/${clickedHill.data[0]._id}`);
-            console.log(hillReviews.data)
-
+            let starMath = 0;
+            hillReviews.data.map((review) => {
+                console.log(review.stars)
+                starMath += review.stars;
+            })
+            await setRating(Math.floor(starMath / hillReviews.data.length));
 
             {
                 user.hills
@@ -89,7 +110,50 @@ function MapPage() {
     // noinspection JSValidateTypes
     return (
         <>
-            <Sidebar hill={currentHill} climbed={climbed} user={user}></Sidebar>
+            {currentHill && <div className={'sidebar'}>
+                <div className={'hill'}>
+                    {currentHill.name}<br/>
+                    {currentHill.elevation}<br/>
+                    {currentHill.lat}<br/>
+                    {currentHill.lon}<br/>
+                    {currentHill.prominence}<br/>
+                    {currentHill.isolation}<br/>
+                    {currentHill.material}<br/>
+                    {currentHill.basin}<br/>
+                    {currentHill.district}<br/>
+                    {currentHill.location}<br/>
+                </div>
+
+                <button id={'btnClaimHill'} type="button" className="btn btn-success" onClick={addHill}
+                        disabled={climbed}>Pokořit
+                </button>
+
+                <div className={'bottom'}>
+                    <button type="button" className="btn btn-primary"><a href="">Settings</a></button>
+                    <a href="/profile">
+                        <button type="button" className="btn btn-primary">Profile</button>
+                    </a>
+                    <button type="button" className="btn btn-primary"><a href="">Collapse</a></button>
+
+                    <hr/>
+
+                </div>
+
+                <hr/>
+
+                <h1>Rating</h1>
+                <div className={'rating'}>
+                    <Rating
+                        name="simple-controlled"
+                        value={rating}
+                        onChange={(event, newValue) => {
+                            setRating(newValue);
+                        }}
+                    />
+                    <button type="button" className="btn btn-warning" onClick={sendRating}>Odeslat</button>
+                </div>
+            </div>
+            }
 
             {user.isAdmin ? <a href={'/admin'}>
                 <button type="button" className="btn btn-success btnAdmin">Admin</button>
