e03a9b59d0e0ec85560fabfadfce023ab81e1ad6
jankubatt <jankubat77@gmail.com>
2022-09-30 12:42:32 +0200

    refactor(admin): format

--- frontend/src/pages/admin.js
+++ frontend/src/pages/admin.js
@@ -1,19 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import '../App.css';
 import axios from "axios";
-import {Card, CardContent, Chip, Rating} from "@mui/material";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import { Collapse } from '@mui/material';
+import {    Card, 
+            CardContent,  
+            Rating, 
+            Paper, 
+            Table, 
+            TableBody, 
+            TableCell, 
+            TableContainer, 
+            TableHead, 
+            TablePagination, 
+            TableRow, 
+            CssBaseline 
+} from '@mui/material/';
 
+//Main table columns
 const columns = [
   { id: 'id_user', label: 'ID', minWidth: 170 },
   { id: 'login', label: 'Login', minWidth: 100 },
@@ -28,10 +31,12 @@ const columns = [
   { id: 'isAdmin', label: 'Admin', minWidth: 100 },
 ];
 
+//formats pushable row into table
 function createData(id_user, login, name, email, desc, hills, comments, reviews, date_registered, date_lastLogin, isAdmin) {
   return { id_user, login, name, email, desc, hills, comments, reviews, date_registered, date_lastLogin, isAdmin };
 }
 
+//dark theme
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
@@ -41,46 +46,51 @@ const darkTheme = createTheme({
 axios.defaults.withCredentials = true;
 
 function AdminPage() {
-    const [users, getUsers] = useState([]);
-    const [hills, getHills] = useState([]);
-    const [allReviews, getAllReviews] = useState([]);
-    const [userHills, setUserHills] = useState([]);
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-    const [rows, setRows] = useState([]);
+    const [users, getUsers] = useState([]);                     //All users
+    const [hills, getHills] = useState([]);                     //All hills
+    const [allReviews, getAllReviews] = useState([]);           //All reviews
+    const [userHills, setUserHills] = useState([]);             //Array where hill is assigned to user
+    const [page, setPage] = React.useState(0);                  //Page of a table
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);   //Row per page of a table
+    const [rows, setRows] = useState([]);                       //Rows of a table
 
+    //Handle page change of a table
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
 
+    //Handle rows per page of a table
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
 
-    let count = 0;
-
+    //Fetch users from database
     const fetchUsers = async () => {
         const response = await axios.get('http://localhost:8082/api/users/');
         return response.data;
     }
 
+    //Fetch hills from database
     const fetchHills = async () => {
         const response = await axios.get('http://localhost:8082/api/hills/');
         return response.data;
     }
 
+    //Fetch reviews from database
     const fetchReviews = async () => {
         const response = await axios.get(`http://localhost:8082/api/review/`);
         return response.data;
     }
 
+    //Fetching data
     useEffect(() => {
         fetchUsers().then((res) => {getUsers(res)})
         fetchHills().then((res) => {getHills(res)})
         fetchReviews().then((res) => {getAllReviews(res)})
     }, [])
 
+    //Assign hills to each user
     useEffect(() => {
         let temp = [];
         users.forEach((user) => {
@@ -96,12 +106,14 @@ function AdminPage() {
         setUserHills(temp);
     }, [hills, users])
 
+    //Fill table with data
     useEffect(() => {
         let tempRows = [];
-        let userClimbed = [];
-        let userReviews = [];
 
         users.map((user) => {
+            let userClimbed = [];
+            let userReviews = [];
+
             userHills.map((hill) => {
                 if (hill.user === user.login) {
                     userClimbed.push(<li>{hill.hill.name}</li>);
@@ -110,32 +122,30 @@ function AdminPage() {
 
             allReviews.map((review) => {
                 if (review.user.login === user.login) {
-                    console.log(review);
-                    userReviews.push(<Card key={review._id} className='card'>
-                    <CardContent>
-                        <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                            <div>
-                                
-                            </div>
-                            <div>
-                                <Rating name="read-only" value={review.stars} readOnly />
-                            </div>
-                        </div>
-                        <div>
-                            {ellipsify(review.text)}
-                        </div>
-                        <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-                            <div style={{color: 'GrayText'}}>
-                                {new Date(review.date_added).getDate()}.{new Date(review.date_added).getMonth()+1}.{new Date(review.date_added).getFullYear()}
-                            </div>
-                        </div>
-                    </CardContent>
-                </Card>
-                                    );
+                    userReviews.push(
+                        <Card key={review._id} className='card'>
+                            <CardContent>
+                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                    <div>
+                                        <Rating name="read-only" value={review.stars} readOnly />
+                                    </div>
+                                </div>
+                                <div>
+                                    {ellipsify(review.text)}
+                                </div>
+                                <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                                    <div style={{color: 'GrayText'}}>
+                                        {new Date(review.date_added).getDate()}.{new Date(review.date_added).getMonth()+1}.{new Date(review.date_added).getFullYear()}
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    );
                 }
             })
 
             tempRows.push(createData(user._id, user.login, user.name, user.email, user.description, userClimbed, "In Progress", userReviews, DateTime(user.date_registered), DateTime(user.date_lastLogin), ((user.isAdmin) ? 'true' : 'false')))
+            
             userClimbed = [];
             userReviews = [];
         })
@@ -145,59 +155,52 @@ function AdminPage() {
 
     return (
         <>
-        <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-        <div className={'container-fluid'}>
-        <a href="#sidebar" data-bs-target="#sidebar" data-bs-toggle="collapse" className="border rounded-3 p-1 text-decoration-none"><i className="bi bi-list bi-lg py-2 p-1"></i> Menu</a>
-        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-            <TableContainer>
-                <Table stickyHeader aria-label="sticky table">
-                <TableHead>
-                    <TableRow>
-                    {columns.map((column) => (
-                        <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ minWidth: column.minWidth }}
-                        >
-                        {column.label}
-                        </TableCell>
-                    ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((row) => {
-                        return (
-                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                            {columns.map((column) => {
-                            const value = row[column.id];
-                            return (
-                                <TableCell key={column.id} align={column.align}>
-                                {column.format && typeof value === 'number'
-                                    ? column.format(value)
-                                    : value}
-                                </TableCell>
-                            );
-                            })}
-                        </TableRow>
-                        );
-                    })}
-                </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[10, 25, 100]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-            </Paper>
-            </div>
+            <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+                <div className={'container-fluid'}>
+                    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                        <TableContainer>
+                            <Table stickyHeader aria-label="sticky table">
+                                <TableHead>
+                                    <TableRow>
+                                        {columns.map((column) => (
+                                            <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
+                                                {column.label}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {rows
+                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map((row) => {
+                                        return (
+                                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                                {columns.map((column) => {
+                                                    const value = row[column.id];
+                                                    return (
+                                                        <TableCell key={column.id} align={column.align}>
+                                                            {column.format && typeof value === 'number' ? column.format(value): value}
+                                                        </TableCell>
+                                                    );
+                                                })}
+                                            </TableRow>
+                                        );
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <TablePagination
+                            rowsPerPageOptions={[10, 25, 100]}
+                            component="div"
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
+                    </Paper>
+                </div>
             </ThemeProvider>
         </>
     )
