c7cc10cdfd9b30fce58346fdc94d75366d1b77f5
jankubatt <jankubat77@gmail.com>
2022-11-06 22:32:14 +0100

    chore(app): code structure

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -66,70 +66,74 @@ const Sidebar = (props) => {
 
     return (
         <div className={'sidebar'}>
-                <div className={'hill'}>
-                    <h1>{props.currentHill.name}<small style={{fontSize: 'medium'}}>({props.currentHill.elevation}m)</small></h1>
-                    <hr/>
-                    <div style={{
-                        width: "100%",
-                        height: "200px",
-                        backgroundImage: `url(${tryHillImage()})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center"
-                    }}>
-
-                    </div>
-                    <h2>Informace</h2>
-                    <div>
-                        {props.currentHill.lat}<br/>
-                        {props.currentHill.lon}<br/>
-                        {props.currentHill.prominence}<br/>
-                        {props.currentHill.isolation}<br/>
-                        {props.currentHill.material}<br/>
-                        {props.currentHill.basin}<br/>
-                        {props.currentHill.district}<br/>
-                        {props.currentHill.location}<br/>
-                    </div>
-                </div>
-
-                {props.currentHill.difficulty.length > 0 ? "Obtížné " : ""}
-                {props.currentHill.path.length > 0 ? "Dostupná cesta " : ""}
-                {props.currentHill.food.length > 0 ? "Vhodné pro kočárky " : ""}
-                {props.currentHill.parking.length > 0 ? "Parkoviště " : ""}
-                {props.currentHill.stroller.length > 0 ? "Občerstvení " : ""}<br />
-
-                <div style={{textAlign: 'center'}}>
-                    <button id={'btnClaimHill'} type="button" className="btn" onClick={addHill}
-                            disabled={props.climbed}>Pokořit
-                    </button>
-                </div>
-
+            <div className={'hill'}>
+                <h1>{props.currentHill.name}<small style={{fontSize: 'medium'}}>({props.currentHill.elevation}m)</small></h1>
+                
                 <hr/>
+                
+                <div style={{
+                    width: "100%",
+                    height: "200px",
+                    backgroundImage: `url(${tryHillImage()})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                    }}></div>
+                    
+                <h2>Informace</h2>
+                    
+                <div>
+                    {props.currentHill.lat}<br/>
+                    {props.currentHill.lon}<br/>
+                    {props.currentHill.prominence}<br/>
+                    {props.currentHill.isolation}<br/>
+                    {props.currentHill.material}<br/>
+                    {props.currentHill.basin}<br/>
+                    {props.currentHill.district}<br/>
+                    {props.currentHill.location}<br/>
+                </div>
+            </div>
 
-                <h1>Rating</h1>
-                <div className={'rating'}>
-                    <Rating name="size-large simple-controlled" value={props.rating || 0} onChange={(event, newValue) => {
+            {props.currentHill.difficulty.length > 0 ? "Obtížné " : ""}
+            {props.currentHill.path.length > 0 ? "Dostupná cesta " : ""}
+            {props.currentHill.food.length > 0 ? "Vhodné pro kočárky " : ""}
+            {props.currentHill.parking.length > 0 ? "Parkoviště " : ""}
+            {props.currentHill.stroller.length > 0 ? "Občerstvení " : ""}<br />
+
+            <div style={{textAlign: 'center'}}>
+                <button id={'btnClaimHill'} type="button" className="btn" onClick={addHill}
+                        disabled={props.climbed}>Pokořit
+                </button>
+            </div>
+
+            <hr/>
+
+            <h1>Rating</h1>
+            
+            <div className={'rating'}>
+                <Rating name="size-large simple-controlled" value={props.rating || 0} onChange={(event, newValue) => {
                         props.setRating(newValue);
                         props.setTxtArea('block')
-                    }} size={'large'}/><br/>
+                    }} size={'large'}
+                /><br/>
 
-                    <button type="button" className="btn" onClick={sendRating}>Odeslat</button><br/>
+                <button type="button" className="btn" onClick={sendRating}>Odeslat</button><br/>
 
-                    <div style={{display: props.txtArea}}>
-                        <Checkbox ref={chbDifficulty} /> Obtížné <br/>
-                        <Checkbox ref={chbPath} /> Dostupná cesta <br/>
-                        <Checkbox ref={chbStroller} /> Vhodné pro kočárky <br/>
-                        <Checkbox ref={chbParking} /> Parkoviště <br/>
-                        <Checkbox ref={chbFood} /> Občerstvení <br/>
-                    </div>
+                <div style={{display: props.txtArea}}>
+                    <Checkbox ref={chbDifficulty} /> Obtížné <br/>
+                    <Checkbox ref={chbPath} /> Dostupná cesta <br/>
+                    <Checkbox ref={chbStroller} /> Vhodné pro kočárky <br/>
+                    <Checkbox ref={chbParking} /> Parkoviště <br/>
+                    <Checkbox ref={chbFood} /> Občerstvení <br/>
+                </div>
 
-                    <textarea ref={reviewText} style={{'width': '20vw', height: '20vh', display: props.txtArea}}></textarea><br/>
+                <textarea ref={reviewText} style={{'width': '20vw', height: '20vh', display: props.txtArea}}></textarea><br/>
                     
-                    <div id='reviews'>
-                        {props.reviews?.map((review) => ((review.text !== null) ? <div key={review._id}><Card className='card'>
-                            <CardContent>
-                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                                    <div>
-                                        <b style={{fontSize: '1.25em'}}>{review.user.name || review.user.login}</b>&nbsp;
+                <div id='reviews'>
+                    {props.reviews?.map((review) => ((review.text !== null) ? <div key={review._id}><Card className='card'>
+                        <CardContent>
+                            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                <div>
+                                    <b style={{fontSize: '1.25em'}}>{review.user.name || review.user.login}</b>&nbsp;
                                         {((review.user.isAdmin) ? <Chip color="error" label="Admin"/> : '')}
                                     </div>
                                     <div><Rating name="read-only" value={review.stars} readOnly /></div>
