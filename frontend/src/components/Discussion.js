5f34c178d3c6811204b74213d5606206432bc277
jankubatt <jankubat77@gmail.com>
2022-11-09 16:58:35 +0100

    feat(app): discussion, part 2

---
+++ frontend/src/components/Discussion.js
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+
+const Discussion = () => {
+    return (
+            <div className="discussion">
+                <p>Lorem Ipsum</p>
+            </div>
+    )
+}
+
+export default Discussion;
--- frontend/src/pages/discussion.js
+++ frontend/src/pages/discussion.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import '../App.css';
+import Discussion from '../components/Discussion';
+import { TextField, Container, Button } from '@mui/material';
 
 function DiscussionPage() {
     return (
         <>
-           <div className='container'>
-                <div className='navbar'>
-                    <div className='navbrand'>Kopcuj.cz</div>
-                    <div className='navbrand' style={{marginRight: "20px"}}>Diskuze</div>
+            <div className='navbar'>
+                <div className='navbrand'>Kopcuj.cz</div>
+                <div className='navbrand' style={{marginRight: "20px"}}>Diskuze</div>
+            </div>
+
+            <Container maxWidth="lg">
+                <div className='formDiscussion'>
+                    <h1>Vytvořit diskuzi</h1>
+                    
+                    <TextField id="outlined-basic" style={{width: "50%"}} label="Téma" variant="outlined" /><br/>
+                    <TextField label="Myšlenka" multiline rows={10} maxRows={10} style={{marginTop: "10px", width: "100%"}} /><br/>
+                    <Button variant="contained" style={{marginTop: "10px"}}>Vytvořit</Button>
                 </div>
 
-                                
-           </div>
+                <hr/>
+
+                <Discussion />                
+            </Container>
         </>
     )
 }
